d5367e9fabd1aa93e0b15beda368dd0d8c138b0b
kekwait <kekwait@users.noreply.github.com>
2020-09-19 13:04:20 +0600

    Upd

--- program.py
+++ program.py
@@ -10,7 +10,17 @@ def fact(n):
 def pow(n, a):
 	return n**a
 
+def simple_check(n):
+	c = 0
+	for i in range (1,n):
+		if n%i==0:
+			c+=1
+		else:
+			continue
+	return c==1
+
 n = int(input("Введите число: "))
 print(fact(n))
 print(pow(n, 5))
+print(simple_check(n))
 
